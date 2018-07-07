0b8479ba4aca6edd749adb3cd9323932e7d320de
Julio Cesar Ferreira <jcferreira@gmail.com>
2018-07-07 15:35:46 -0300

    persistindo Role do usuario login

--- src/main/java/br/com/intelipost/integration/domain/UsuarioLogin.java
+++ src/main/java/br/com/intelipost/integration/domain/UsuarioLogin.java
@@ -1,9 +1,15 @@
 package br.com.intelipost.integration.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -16,6 +22,17 @@ public class UsuarioLogin {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    
+    @Column(nullable=false, unique=true)
+    private String email;
 
+    @Column(nullable=false)
+    private String nome;
 
+    @Column(nullable=false)
+    private String senha;
+
+    @OneToMany(fetch=FetchType.EAGER)
+    private List<Role> permissoes = new ArrayList<Role>();
+    
 }
