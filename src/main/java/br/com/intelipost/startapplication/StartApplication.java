a8da7f14154da4fe1b6a5dc785fe09ee081812a6
Julio Cesar Ferreira <jcferreira@gmail.com>
2018-07-07 14:38:47 -0300

    adicionando dependencias pom.xml e configuracoes de banco H2

--- src/main/java/br/com/intelipost/startapplication/StartApplication.java
+++ src/main/java/br/com/intelipost/startapplication/StartApplication.java
@@ -2,10 +2,12 @@ package br.com.intelipost.startapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class StartApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
